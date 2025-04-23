d3c47a1596ea299c5fc7b73be2bfbdb1f0b23654
César Henrique <cesaarhvb@gmail.com>
2025-04-23 02:06:04 +0000

    feat: nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,8 @@ int main() {
     float area1;
     float pib1;
     int pontos_turisticos1;
+    float densidade_pop1;
+    float pib_per_capita1;
     
     // Variáveis para a segunda carta
     char estado2;
@@ -18,6 +20,8 @@ int main() {
     float area2;
     float pib2;
     int pontos_turisticos2;
+    float densidade_pop2;
+    float pib_per_capita2;
     
     // Cadastro da primeira carta
     printf("Cadastro da Carta 1\n");
@@ -52,6 +56,13 @@ int main() {
     scanf("%f", &pib2);
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontos_turisticos2);
+
+    // Cálculo das propriedades
+    densidade_pop1 = populacao1 / area1;
+    pib_per_capita1 = pib1 / populacao1;
+
+    densidade_pop2 = populacao2 / area2;
+    pib_per_capita2 = pib2 / populacao2;
     
     // Exibição dos dados da primeira carta
     printf("\nCarta 1:\n");
@@ -62,6 +73,8 @@ int main() {
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_pop1);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita1);
     
     // Exibição dos dados da segunda carta
     printf("\nCarta 2:\n");
@@ -72,6 +85,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_pop2);
+    printf("PIB per Capita: %.2f reais\n", pib_per_capita2);
     
     return 0;
 }
